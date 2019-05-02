d57293a689c1d469651b40ed83566756cf2381c8
App Academy Student <patrickpondo7@gmail.com>
2019-05-02 16:52:18 -0400

    Finish Warm-up and Band mvc for Music App

---
+++ music_app/app/models/user.rb
@@ -0,0 +1,51 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string
+#  password_digest :string
+#  session_token   :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+    validates :email, :password_digest, :session_token, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: { minimum: 6} , allow_nil: true
+    
+    after_initialize :ensure_session_token
+
+    attr_reader :password
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        return nil unless user && user.is_password?(password)
+        user
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        bcrypt = BCrypt::Password.new(self.password_digest)
+        bcrypt.is_password?(password)
+    end
+
+    def reset_session_token!
+        self.update!(session_token: self.class.generate_session_token)
+        self.session_token
+    end
+
+    private
+    def ensure_session_token
+        self.session_token ||= self.class.generate_session_token
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+end
---
+++ music_app/db/migrate/20190502181511_remove_column_from_bands.rb
@@ -0,0 +1,4 @@
+class RemoveColumnFromBands < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
